4bb93d739c87d15398b5a87a5177dd59e867fd8b
Marcos Antônio <marcoscardoso.programmer@gmail.com>
2024-10-02 07:48:32 -0300

    fix(Fila de Pedidos)

--- Base.java
+++ Base.java
@@ -1,25 +1,18 @@
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Base
- */
 public class Base {
     public static final int NUM_CLIENTES = 5;
     public static final int NUM_CONSUMIDORES = 10;
+    public static final int TAMANHO_FILA = 10;
 
-    BlockingQueue<Pedido> queue = new ArrayBlockingQueue<>(10);
+    BlockingQueue<Pedido> queue = new ArrayBlockingQueue<>(TAMANHO_FILA);
     ScheduledExecutorService clientes = Executors.newScheduledThreadPool(NUM_CLIENTES);
     ScheduledExecutorService consumidores = Executors.newScheduledThreadPool(NUM_CONSUMIDORES);
-
-    public Base() {
-
-    }
 
     public void run() {
         // TODO FUTURO: Trocar ScheduledThreadPool de clientes para CachedThreadPool
@@ -29,15 +22,14 @@ public class Base {
         }
 
         // Processar Pedidos (consumidores) | Faça semelhante aos clientes
-        for (int i = 0; i < NUM_CLIENTES; i++) {
-            clientes.scheduleAtFixedRate(new Cliente(queue), 0, new Random().nextInt(5) + 1, TimeUnit.SECONDS);
+        for (int i = 0; i < NUM_CONSUMIDORES; i++) {
+            consumidores.scheduleAtFixedRate(/* new Cliente(queue) */ null, 0, new Random().nextInt(5) + 1,
+                    TimeUnit.SECONDS);
         }
 
         // Reestocar | Single thread para reestocar.
 
         // TODO: Fazer relatório na thread principal.
-        for (;;) {
-        }
 
         // clientes.shutdown();
     }
