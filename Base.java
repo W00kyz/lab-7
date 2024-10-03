e7029503be8ab2944c7aed2813ee4d0f08b367e3
VictorFreir <victorvfreire@gmail.com>
2024-10-03 17:41:48 -0300

    recompondo estoque pronto

--- Base.java
+++ Base.java
@@ -13,13 +13,31 @@ public class Base {
     BlockingQueue<Pedido> queue = new ArrayBlockingQueue<>(TAMANHO_FILA);
     ScheduledExecutorService clientes = Executors.newScheduledThreadPool(NUM_CLIENTES);
     ScheduledExecutorService consumidores = Executors.newScheduledThreadPool(NUM_CONSUMIDORES);
+    ScheduledExecutorService schedulerEstoque = Executors.newScheduledThreadPool(1);
+
+    Estoque estoque = new Estoque();
+
 
     public void run() {
+        estoque.adicionarProduto(NomesProdutos.ARROZ, 1);
+        estoque.adicionarProduto(NomesProdutos.FEIJAO, 1);
+        estoque.adicionarProduto(NomesProdutos.MACARRAO, 1);
+        estoque.adicionarProduto(NomesProdutos.LEITE, 1);
+        estoque.adicionarProduto(NomesProdutos.CAFÉ, 1);
+        estoque.adicionarProduto(NomesProdutos.AÇÚCAR, 1);
+        estoque.adicionarProduto(NomesProdutos.SAL, 1);
+        estoque.adicionarProduto(NomesProdutos.ÓLEO, 1);
+        estoque.adicionarProduto(NomesProdutos.FARINHA, 1);
+        estoque.adicionarProduto(NomesProdutos.MANTEIGA, 1);
+
         // TODO FUTURO: Trocar ScheduledThreadPool de clientes para CachedThreadPool
         // após fazer consumidores.
+        
         for (int i = 0; i < NUM_CLIENTES; i++) {
             clientes.scheduleAtFixedRate(new Cliente(queue), 0, new Random().nextInt(5) + 1, TimeUnit.SECONDS);
         }
+
+        schedulerEstoque.scheduleAtFixedRate(estoque::recomporEstoque, 0, 2, TimeUnit.SECONDS);
 
         // Processar Pedidos (consumidores) | Faça semelhante aos clientes
         for (int i = 0; i < NUM_CONSUMIDORES; i++) {
@@ -28,7 +46,7 @@ public class Base {
         }
 
         // Reestocar | Single thread para reestocar.
-
+        
         // TODO: Fazer relatório na thread principal.
 
         // clientes.shutdown();
--- Estoque.java
+++ Estoque.java
@@ -1,14 +1,29 @@
 import java.util.concurrent.*;
 
 public class Estoque {
-    private final ConcurrentHashMap<String, Integer> estoque = new ConcurrentHashMap<>();
-    
-    public int consultarEstoque(String item) {
+    // nome : quantidade (int)
+    private final ConcurrentHashMap<NomesProdutos, Integer> estoque = new ConcurrentHashMap<>();
+
+    public int consultarEstoque(NomesProdutos item) {
         return estoque.getOrDefault(item, 0);
     }
 
-    public void atualizarEstoque(String item, int novaQuantidade) {
-        estoque.put(item, novaQuantidade);
+    public void atualizarEstoque(NomesProdutos item, int quantidade) {
+        estoque.put(item, quantidade);
     }
 
+    public void removerProduto(NomesProdutos item) {
+        estoque.remove(item);
+    }
+
+    public void recomporEstoque() {
+        System.out.println("Recompondo estoque...");
+        estoque.forEach((item, quantidade) -> {
+            estoque.put(item, quantidade + 10);
+        });
+    }
+
+    public void adicionarProduto(NomesProdutos item, int quantidade) {
+        estoque.put(item, quantidade);
+    }
 }
